32efd78791dfd114237821bf78c0ba523547a50e
mykhailopulov <mihail.pulov.247@gmail.com>
2026-02-04 13:30:55 +0200

    Added String faculty for Student

--- src/Student.java
+++ src/Student.java
@@ -1,7 +1,9 @@
 class Student extends Person {
     private int course;
     private int group;
+    private String faculty;
 
     public int getCourse() {return course;}
     public int getGroup() {return group;}
+    public String getFaculty() {return faculty;}
 }
