ccf4b386e8c4f3010503665ded2f26f5b5a81b55
mykhailopulov <mihail.pulov.247@gmail.com>
2026-02-11 10:59:37 +0200

    Added Person.getOnlyName, removed Person.fullName, now creating while Person.getName and Person.getFullName

--- src/Person.java
+++ src/Person.java
@@ -2,28 +2,25 @@
 public class Person implements NamedEntity {
     private String name;
     private String surname;
-    private String fullName;
 
     public Person(String name, String surname) {
         this.name = name;
         this.surname = surname;
-        this.fullName = surname + " " + name;
     }
 
     @Override
     public String getName() {
-        return this.fullName;
+        return this.name+" "+ this.surname;
     }
     public String getSurname() {return surname;}
-    public String getFullName() {return fullName;}
+    public String getOnlyName() {return this.name;}
+    public String getFullName() {return this.name+" "+ this.surname;}
 
     public void setSurname(String newSurname) {
         this.surname = newSurname;
-        this.fullName = newSurname + " " + name;
     }
 
     public void setName(String newName) {
         this.name = newName;
-        this.fullName = surname + " " + newName;
     }
 }
